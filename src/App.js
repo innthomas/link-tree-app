6c664d7befa4c1c088eab340cb21e075bfcba3cc
innthomas <innthomas00158@gmail.com>
2022-10-30 13:43:45 +0100

    spinned up react app with tailwindcss, added header(photo and name)  to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import innPhoto from './assets/innocent-thomas.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App '>
+       <div className='flex flex-col content-center items-center m-2'>
+         <img className='App__innPhoto w-20 h-20 rounded-full m-2' src={innPhoto} alt="innPhoto" />
+         <h1 className='text-2xl font-bold m-2'>Innocent Thomas</h1>
+       </div>
     </div>
   );
 }
